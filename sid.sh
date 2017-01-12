c6581438941f095fe48851bde4e213858aa9ee95
Siddhu <Siddharth16reddy@gmail.com>
2017-01-12 03:57:31 +0000

    added email and files count

--- sid.sh
+++ sid.sh
@@ -9,9 +9,13 @@ cd /home/ec2-user/shell
 r=`date +%Y%m%d\-%H%M`
 mkdir $r
 #echo `date +%Y%m%d\-%H%M%S`- new $r created>>/tmp/log/$r1
+
+ 
 cp -rf ram/* $r
+a=`ls -l ram|grep ^-|wc -l`
+echo $a
 cd /tmp/log
-echo `date +%Y%m%d\-%H%M%S`- 4 files copied>>$r1
+echo `date +%Y%m%d\-%H%M%S`- $a files copied>>$r1
 
 echo `date +%Y%m%d\-%H%M%S`- script ending>>$r1
- 
+cat /tmp/log/$r1|mailx siddharth16reddy@gmail.com 
