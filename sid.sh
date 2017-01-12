836c0e2b0259a26e7ddc1a6053d7f17827ecbb38
Siddhu <Siddharth16reddy@gmail.com>
2017-01-12 19:13:54 +0000

    added auto increment load

--- sid.sh
+++ sid.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-
 cd /tmp/log
 r1=`date +%Y%m%d\-%H%M%S`
-echo $r1 
+echo $r1
 
 echo `date +%Y%m%d%H%M%S`- script started>>$r1
 cd /home/ec2-user/shell
@@ -10,12 +9,29 @@ r=`date +%Y%m%d\-%H%M`
 mkdir $r
 #echo `date +%Y%m%d\-%H%M%S`- new $r created>>/tmp/log/$r1
 
- 
-cp -rf ram/* $r
-a=`ls -l ram|grep ^-|wc -l`
+cp -rf /home/ec2-user/shell/ram/* /home/ec2-user/shell/ram2
+for file in `ls /home/ec2-user/shell/ram1`
+do
+for f in `ls /home/ec2-user/shell/ram`
+do
+if [ "$f" == "$file" ]
+then
+rm -f /home/ec2-user/shell/ram2/$file
+continue
+fi
+done
+done
+
+
+
+cp -rf /home/ec2-user/shell/ram2/* $r
+a=`ls -l ram2|grep ^-|wc -l`
 echo $a
 cd /tmp/log
 echo `date +%Y%m%d\-%H%M%S`- $a files copied>>$r1
 
 echo `date +%Y%m%d\-%H%M%S`- script ending>>$r1
+echo `ls /home/ec2-user/shell/ram2`
+cp -rf /home/ec2-user/shell/ram2/* /home/ec2-user/shell/ram1
+rm -rf /home/ec2-user/shell/ram2/*
 cat /tmp/log/$r1|mailx siddharth16reddy@gmail.com, rahul.varakala@gmail.com 
