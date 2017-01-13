7fe3a140666bc2c63ca6f860fea283b23edf0897
Siddhu <Siddharth16reddy@gmail.com>
2017-01-13 16:50:06 +0000

    Removed auto increment

--- sid.sh
+++ sid.sh
@@ -1,55 +1,21 @@
 #!/bin/sh
-#list of directories and file addresses
-log_dir=/tmp/log
-out_dir=/home/ec2-user/shell
-copy_history=/home/ec2-user/shell/prev_copied_files
-my_input=/home/ec2-user/shell/ram
-tempfile=/home/ec2-user/shell/tempfile
 
-cd $log_dir
-dname=`date +%Y%m%d\-%H%M%S`
-echo $dname
+cd /tmp/log
+r1=`date +%Y%m%d\-%H%M%S`
+echo $r1 
 
-#creating output directory
-echo `date +%Y%m%d%H%M%S`- script started>>$dname
-cd $out_dir
-mkdir $dname
+echo `date +%Y%m%d%H%M%S`- script started>>$r1
+cd /home/ec2-user/shell
+r=`date +%Y%m%d\-%H%M`
+mkdir $r
+#echo `date +%Y%m%d\-%H%M%S`- new $r created>>/tmp/log/$r1
 
-#comparing input directory files with previous copied files and storing uncopied files name into tempfile
-for line in `ls -1 $my_input`
-do
-    echo $line>>$tempfile
-done
-echo $tempfile
-echo $my_input
-cat $tempfile
-for file in `cat $copy_history`
-do
-  for f in `ls -1 $my_input`
-  do
-    if [ "$f" == "$file" ]
-    then
-        sed -i "/\b$f\b/d" $tempfile
-        continue
-        fi
-  done
-done
-#copying uncopied files into output directory and updating previous copied file
-for i in `cat $tempfile`
-do
-  cp -rf $my_input/$i $out_dir/$dname
-  echo $i>>$copy_history
-done
-#listing out the count of newly copied files and subdirectories
-file_count=`ls -l $out_dir/$dname |grep ^-|wc -l`
-subdir_count=`ls -l $out_dir/$dname |grep ^d|wc -l`
+ 
+cp -rf ram/* $r
+a=`ls -l ram|grep ^-|wc -l`
 echo $a
-cd $log_dir
-echo `date +%Y%m%d\-%H%M%S`- $file_count files copied>>$dname
-echo `date +%Y%m%d\-%H%M%S`- $subdir_count subdir copied>>$dname
-echo `date +%Y%m%d\-%H%M%S`- script ending>>$dname
+cd /tmp/log
+echo `date +%Y%m%d\-%H%M%S`- $a files copied>>$r1
 
-# deleting contents of tempfile and sending an email to recepient
-echo $tempfile
-echo -n>$tempfile
-cat $log_dir/$dname|mailx siddharth16reddy@gmail.com
+echo `date +%Y%m%d\-%H%M%S`- script ending>>$r1
+cat /tmp/log/$r1|mailx siddharth16reddy@gmail.com, rahul.varakala@gmail.com 
