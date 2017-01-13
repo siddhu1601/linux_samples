dded8debd412ed57668192b16c74859d394d9771
Siddhu <Siddharth16reddy@gmail.com>
2017-01-13 01:36:21 +0000

    modified code with new files

--- sid.sh
+++ sid.sh
@@ -1,37 +1,52 @@
 #!/bin/sh
-cd /tmp/log
-r1=`date +%Y%m%d\-%H%M%S`
-echo $r1
+log_dir=/tmp/log
+out_dir=/home/ec2-user/shell
+copy_history=/home/ec2-user/shell/prev_copied_files
+my_input=/home/ec2-user/shell/ram
+tempfile=/home/ec2-user/shell/tempfile
 
-echo `date +%Y%m%d%H%M%S`- script started>>$r1
-cd /home/ec2-user/shell
-r=`date +%Y%m%d\-%H%M`
-mkdir $r
-#echo `date +%Y%m%d\-%H%M%S`- new $r created>>/tmp/log/$r1
+cd $log_dir
+dname=`date +%Y%m%d\-%H%M%S`
+echo $dname
 
-cp -rf /home/ec2-user/shell/ram/* /home/ec2-user/shell/ram2
-for file in `ls /home/ec2-user/shell/ram1`
+echo `date +%Y%m%d%H%M%S`- script started>>$dname
+cd $out_dir
+mkdir $dname
+
+for line in `ls -1 $my_input`
 do
-for f in `ls /home/ec2-user/shell/ram`
+    echo $line>>$tempfile
+done
+echo $tempfile
+echo $my_input
+cat $tempfile
+for file in `cat $copy_history`
 do
-if [ "$f" == "$file" ]
-then
-rm -f /home/ec2-user/shell/ram2/$file
-continue
-fi
+  for f in `ls -1 $my_input`
+  do
+    if [ "$f" == "$file" ]
+    then
+        sed -i "/\b$f\b/d" $tempfile
+        continue
+        fi
+  done
 done
+
+for i in `cat $tempfile`
+do
+  cp -rf $my_input/$i $out_dir/$dname
+  echo $i>>$copy_history
 done
 
-
-
-cp -rf /home/ec2-user/shell/ram2/* $r
-a=`ls -l ram2|grep ^-|wc -l`
+file_count=`ls -l $out_dir/$dname |grep ^-|wc -l`
+subdir_count=`ls -l $out_dir/$dname |grep ^d|wc -l`
 echo $a
-cd /tmp/log
-echo `date +%Y%m%d\-%H%M%S`- $a files copied>>$r1
+cd $log_dir
+echo `date +%Y%m%d\-%H%M%S`- $file_count files copied>>$dname
+echo `date +%Y%m%d\-%H%M%S`- $subdir_count subdir copied>>$dname
 
-echo `date +%Y%m%d\-%H%M%S`- script ending>>$r1
-echo `ls /home/ec2-user/shell/ram2`
-cp -rf /home/ec2-user/shell/ram2/* /home/ec2-user/shell/ram1
-rm -rf /home/ec2-user/shell/ram2/*
-cat /tmp/log/$r1|mailx siddharth16reddy@gmail.com, rahul.varakala@gmail.com 
+echo `date +%Y%m%d\-%H%M%S`- script ending>>$dname
+echo $tempfile
+echo -n>$tempfile
+
+cat $log_dir/$dname|mailx siddharth16reddy@gmail.com
